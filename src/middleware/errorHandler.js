0a654823cb304df1359e83a781c6efd606439a5d
ggsno <ogs0518@gmail.com>
2023-12-07 03:04:18 +0900

    Fix: samesite none, secure ture

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,8 +1,11 @@
 function errorHandler(err, req, res) {
-  res.status(err.status || 500).json({
-    result: "fail",
-    error: err.message || "Error",
-  });
+  console.log("EE" + err);
+  console.log("QQ" + req);
+  console.log("RR" + res);
+  // res.status(err.status || 500).json({
+  //   result: "fail",
+  //   error: err.message || "Error",
+  // });
 }
 
 export default errorHandler;
